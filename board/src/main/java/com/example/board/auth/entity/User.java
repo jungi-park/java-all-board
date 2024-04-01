a73fd184f7d564bd46136ebffdf1db9dfdc3f9c1
박준기 <qmqqqqm@gmail.com>
2024-04-01 18:34:26 +0900

    User Entity  생성

---
+++ board/src/main/java/com/example/board/auth/entity/User.java
@@ -0,0 +1,24 @@
+package com.example.board.auth.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@Column(name = "userId",unique = true)
+	private String userId;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "password")
+	private String password;
+}
