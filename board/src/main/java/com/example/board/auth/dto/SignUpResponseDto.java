4fe6cc2d0c614379f799a9f4cb7b0dc2dee19a4c
박준기 <qmqqqqm@gmail.com>
2024-04-08 18:22:32 +0900

    회원가입, 로그인 응답 Dto @Schema 추가

--- board/src/main/java/com/example/board/auth/dto/SignUpResponseDto.java
+++ board/src/main/java/com/example/board/auth/dto/SignUpResponseDto.java
@@ -2,14 +2,19 @@ package com.example.board.auth.dto;
 
 import org.springframework.http.HttpStatus;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
+@Schema(description = "회원가입 응답 데이터")
 @Data
 public class SignUpResponseDto {
 
+	@Schema(description = "메시지")
 	private String message;
 
+	@Schema(description = "회원가입된 유저 아이디")
 	private String userId;
 
+	@Schema(description = "응답코드")
 	private HttpStatus httpStatus;
 }
