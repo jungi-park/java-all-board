fe03ae6bf6d479ef7bfff7752e42e5bb40f314b7
박준기 <qmqqqqm@gmail.com>
2024-04-09 17:01:51 +0900

    개인정보 확인 API return 값 예외처리

--- board/src/main/java/com/example/board/auth/controller/AuthContoller.java
+++ board/src/main/java/com/example/board/auth/controller/AuthContoller.java
@@ -55,8 +55,7 @@ public class AuthContoller {
 	@RequestMapping(value = "/user", method = RequestMethod.GET)
 	public User userInfo (Authentication authentication) throws Exception {
 		String userId = (String) authentication.getPrincipal();
-		log.info("userDetails = {}", userId);
-		return userService.findByUserId(userId);
+		return userService.findByUserId(userId).orElseThrow(() -> new UsernameNotFoundException("해당하는 유저를 찾을 수 없습니다."));
 	}
 
 }
