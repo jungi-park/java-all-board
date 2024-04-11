a2a6d8f0244b862d14eb1877f520f26efeec97a4
박준기 <qmqqqqm@gmail.com>
2024-04-11 16:55:10 +0900

    board 객체와 user 객체 N:1 맵핑

--- board/src/main/java/com/example/board/board/entity/Board.java
+++ board/src/main/java/com/example/board/board/entity/Board.java
@@ -7,9 +7,12 @@ import com.example.board.auth.entity.User;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Temporal;
@@ -38,6 +41,8 @@ public class Board {
 
 	private Boolean isDelete;
 
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="USER_ID")
 	private User writer;
 
 	@PrePersist
