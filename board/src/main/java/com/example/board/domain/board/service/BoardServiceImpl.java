4150b30342338a1faf82e5091d96b3ba7b315c35
박준기 <qmqqqqm@gmail.com>
2024-04-26 16:40:05 +0900

    글삭제시 예외타입 RuntimeException로 변경

--- board/src/main/java/com/example/board/domain/board/service/BoardServiceImpl.java
+++ board/src/main/java/com/example/board/domain/board/service/BoardServiceImpl.java
@@ -34,7 +34,7 @@ public class BoardServiceImpl implements BoardService {
 	@Transactional
 	public void deleteBoard(Long boardId, String userId) {
 		Board board = boardRepository.findById(boardId)
-				.orElseThrow(() -> new UsernameNotFoundException("해당하는 게시글을 찾을 수 없습니다."));
+				.orElseThrow(() -> new RuntimeException("해당하는 게시글을 찾을 수 없습니다."));
 		if (board.getWriter().getUserId().equals(userId)) {
 			board.deleteUpdate(true);
 			boardRepository.save(board);
